5bf88130922ed2b6129fef0130c5655a529a9016
Somindras <antonin.juquel@outlook.fr>
2024-01-26 16:32:15 +0100

    manual chunk

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,31 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+const bigLibs = {
+  "@mui": "vendor_mui",
+};
+
+function manualChunks(id: string) {
+  if (id.includes("node_modules")) {
+    for (const [lib, chunk] of Object.entries(bigLibs)) {
+      if (id.includes(lib)) {
+        return chunk;
+      }
+    }
+
+    return "vendor";
+  }
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks,
+      },
+    },
+  },
   plugins: [react()],
-  base: '/AntoninJuquel/'
-})
+  base: "/AntoninJuquel/",
+});
