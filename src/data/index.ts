af57ed403c1defd651745381994bc3130ac69a8a
Somindras <antonin.juquel@outlook.fr>
2024-01-26 18:35:24 +0100

    update data

--- src/data/index.ts
+++ src/data/index.ts
@@ -4,4 +4,25 @@ import experiences from "./experiences.json";
 import projects from "./projects.json";
 import educations from "./educations.json";
 
-export const sections = { about, skills, experiences, projects, educations };
+export const sections = {
+  about: {
+    url: "",
+    data: about,
+  },
+  skills: {
+    url: "https://raw.githubusercontent.com/AntoninJuquel/AntoninJuquel/main/src/data/skills.json",
+    data: skills,
+  },
+  experiences: {
+    url: "https://raw.githubusercontent.com/AntoninJuquel/AntoninJuquel/main/src/data/experiences.json",
+    data: experiences,
+  },
+  projects: {
+    url: "https://raw.githubusercontent.com/AntoninJuquel/AntoninJuquel/main/src/data/projects.json",
+    data: projects,
+  },
+  educations: {
+    url: "https://raw.githubusercontent.com/AntoninJuquel/AntoninJuquel/main/src/data/educations.json",
+    data: educations,
+  },
+};
