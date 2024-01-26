4ec5428101beefdea34927bcb3b01024ad5bf370
Somindras <57025128+AntoninJuquel@users.noreply.github.com>
2024-01-26 15:36:16 +0100

    added project search bar

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -9,6 +9,8 @@ import CardActionArea from "@mui/material/CardActionArea";
 import Typography from "@mui/material/Typography";
 import Stack from "@mui/material/Stack";
 import Chip from "@mui/material/Chip";
+import Autocomplete from "@mui/material/Autocomplete";
+import TextField from "@mui/material/TextField";
 import { useTheme } from "@mui/material/styles";
 
 import Section from "./common/Section";
@@ -22,6 +24,10 @@ const categories = [
   "All",
   ...new Set(projects.map((project) => project.category)),
 ];
+const projectTitles = projects.map((project) => project.title);
+const projectTags = [
+  ...new Set(projects.map((project) => project.tags).flat()),
+];
 
 type Props = {
   modalState: ModalState;
@@ -31,13 +37,49 @@ type Props = {
 function Projects({ setModalState }: Props) {
   const theme = useTheme();
   const [activeCategory, setActiveCategory] = useState(categories[0]);
+  const [activeFilters, setActiveFilters] = useState<string[]>([]);
+
   const projectsToRender = projects.filter((project) => {
-    if (activeCategory === categories[0]) return true;
-    return project.category === activeCategory;
+    const hasFilter = activeFilters.length > 0;
+    const hasMatchingTag = project.tags.some((tag) =>
+      activeFilters.includes(tag)
+    );
+    const hasMatchingTitle = activeFilters.includes(project.title);
+    const hasMatchingCategory =
+      project.category === activeCategory || activeCategory === categories[0];
+
+    if (hasFilter) {
+      return (hasMatchingTag || hasMatchingTitle) && hasMatchingCategory;
+    } else {
+      return hasMatchingCategory;
+    }
   });
+
   return (
     <Section id="projects">
       <Title>Projects</Title>
+      <Autocomplete
+        multiple
+        id="tags-standard"
+        options={[...projectTitles, ...projectTags]}
+        groupBy={(option) => {
+          if (projectTitles.includes(option)) {
+            return "Project Titles";
+          }
+          return "Tags";
+        }}
+        onChange={(_, value) => {
+          setActiveFilters(value);
+        }}
+        renderInput={(params) => (
+          <TextField
+            {...params}
+            variant="standard"
+            label="Search"
+            placeholder="Project title or tag"
+          />
+        )}
+      />
       <Tabs
         value={activeCategory}
         onChange={(_, value) => setActiveCategory(value)}
@@ -48,68 +90,78 @@ function Projects({ setModalState }: Props) {
         ))}
       </Tabs>
       <Grid container spacing={3} mt={4}>
-        {projectsToRender.map((project) => (
-          <Grid item xs={12} sm={6} md={4} key={project.title}>
-            <Card
-              sx={{
-                height: "100%",
-                border: "2px solid transparent",
-                transition: "all .3s ease-in-out",
-                backgroundColor: theme.palette.background.paper,
-                ":hover": {
-                  border: `2px solid ${theme.palette.secondary.main}`,
-                  boxShadow: `${theme.palette.secondary.main} 0px 4px 24px;`,
-                  transform: "scale(1.05)",
-                },
-              }}
-            >
-              <CardActionArea
-                onClick={() => setModalState({ isOpen: true, project })}
+        {projectsToRender.length > 0 ? (
+          projectsToRender.map((project) => (
+            <Grid item xs={12} sm={6} md={4} key={project.title}>
+              <Card
                 sx={{
                   height: "100%",
-                  display: "flex",
-                  flexDirection: "column",
-                  justifyContent: "flex-start",
-                  alignItems: "flex-start",
+                  border: "2px solid transparent",
+                  transition: "all .3s ease-in-out",
+                  backgroundColor: theme.palette.background.paper,
+                  ":hover": {
+                    border: `2px solid ${theme.palette.secondary.main}`,
+                    boxShadow: `${theme.palette.secondary.main} 0px 4px 24px;`,
+                    transform: "scale(1.05)",
+                  },
                 }}
               >
-                <CardMedia
-                  component="img"
-                  image={project.image}
-                  alt={project.title}
-                  sx={{ objectFit: "contain" }}
-                />
-                <Stack
-                  direction="row"
-                  sx={{ padding: "1rem" }}
-                  flexWrap="wrap"
-                  gap={1}
+                <CardActionArea
+                  onClick={() => setModalState({ isOpen: true, project })}
+                  sx={{
+                    height: "100%",
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "flex-start",
+                    alignItems: "flex-start",
+                  }}
                 >
-                  {project.tags.map((tag) => (
-                    <Chip key={tag} label={tag} />
-                  ))}
-                </Stack>
-                <CardHeader title={project.title} subheader={project.date} />
-                <CardContent>
-                  <Typography
-                    variant="body2"
-                    color={theme.palette.text.secondary}
-                    sx={{
-                      textOverflow: "ellipsis",
-                      maxWidth: "100%",
-                      overflow: "hidden",
-                      display: "-webkit-box",
-                      WebkitLineClamp: 3,
-                      WebkitBoxOrient: "vertical",
-                    }}
+                  <CardMedia
+                    component="img"
+                    image={project.image}
+                    alt={project.title}
+                    sx={{ objectFit: "contain" }}
+                  />
+                  <Stack
+                    direction="row"
+                    sx={{ padding: "1rem" }}
+                    flexWrap="wrap"
+                    gap={1}
                   >
-                    {project.description}
-                  </Typography>
-                </CardContent>
-              </CardActionArea>
-            </Card>
-          </Grid>
-        ))}
+                    {project.tags.map((tag) => (
+                      <Chip key={tag} label={tag} />
+                    ))}
+                  </Stack>
+                  <CardHeader title={project.title} subheader={project.date} />
+                  <CardContent>
+                    <Typography
+                      variant="body2"
+                      color={theme.palette.text.secondary}
+                      sx={{
+                        textOverflow: "ellipsis",
+                        maxWidth: "100%",
+                        overflow: "hidden",
+                        display: "-webkit-box",
+                        WebkitLineClamp: 3,
+                        WebkitBoxOrient: "vertical",
+                      }}
+                    >
+                      {project.description}
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+              </Card>
+            </Grid>
+          ))
+        ) : (
+          <Typography
+            variant="body2"
+            color={theme.palette.text.secondary}
+            margin="auto"
+          >
+            No projects found.
+          </Typography>
+        )}
       </Grid>
     </Section>
   );
